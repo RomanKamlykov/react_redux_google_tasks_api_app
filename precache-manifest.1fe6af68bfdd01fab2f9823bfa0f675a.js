4fccbfa5e472c54fa028d4a967375232eed22e03
RomanKamlykov <romankamlikov@gmail.com>
2020-01-18 21:46:47 +0200

    Updates

---
+++ precache-manifest.1fe6af68bfdd01fab2f9823bfa0f675a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d8599d174d58bbf62e7381db6a6452",
+    "url": "/react_redux_google_tasks_api_app/index.html"
+  },
+  {
+    "revision": "6168d0d6d3e52b28e225",
+    "url": "/react_redux_google_tasks_api_app/static/css/main.6c021e9a.chunk.css"
+  },
+  {
+    "revision": "6ea1fb9c99247b86ec7d",
+    "url": "/react_redux_google_tasks_api_app/static/js/2.b8959414.chunk.js"
+  },
+  {
+    "revision": "6168d0d6d3e52b28e225",
+    "url": "/react_redux_google_tasks_api_app/static/js/main.0c4e6117.chunk.js"
+  },
+  {
+    "revision": "9391c99e8c4e8447ebe7",
+    "url": "/react_redux_google_tasks_api_app/static/js/runtime~main.2c7807e9.js"
+  }
+]);
